ee714ec37bd9b5f2d7951c56b6ebeef7c6cdcc0c
Stefano Andriolo <stefano.andriolo@blp-digital.com>
2025-05-02 11:56:53 +0200

    feat: use `DispatchParsedType` in predicate value extractor (#173)

--- frontend/libraries/ballerina-core/src/forms/domains/parser/domains/predicates/domains/extractor/state.ts
+++ frontend/libraries/ballerina-core/src/forms/domains/parser/domains/predicates/domains/extractor/state.ts
@@ -1,14 +1,17 @@
 import { List, Map } from "immutable";
-import { ValueOrErrors, Errors } from "../../../../../../../../main";
-import { ParsedType } from "../../../types/state";
+import {
+  ValueOrErrors,
+  Errors,
+  DispatchParsedType,
+} from "../../../../../../../../main";
 import { PredicateValue } from "../../state";
 
 export const PredicateValueExtractor = {
   Operations: {
     ExtractPredicateValue: (
       lookupName: string,
-      typesMap: Map<string, ParsedType<any>>,
-      t: ParsedType<any>,
+      typesMap: Map<string, DispatchParsedType<any>>,
+      t: DispatchParsedType<any>,
     ): ((
       v: PredicateValue,
     ) => ValueOrErrors<Array<PredicateValue>, Errors<any>>) => {
@@ -34,17 +37,17 @@ export const PredicateValueExtractor = {
         }
         case "primitive":
           return (_) => ValueOrErrors.Default.return([]);
-        case "option": {
-          const traverseOptionValue = self(lookupName, typesMap, t.value);
-          return (v: PredicateValue) =>
-            !PredicateValue.Operations.IsOption(v)
-              ? ValueOrErrors.Default.throwOne(
-                  Errors.Default.singleton(["not a ValueOption", v]),
-                )
-              : !v.isSome
-                ? ValueOrErrors.Default.return([])
-                : traverseOptionValue(v.value);
-        }
+        // case "option": {
+        //   const traverseOptionValue = self(lookupName, typesMap, t.value);
+        //   return (v: PredicateValue) =>
+        //     !PredicateValue.Operations.IsOption(v)
+        //       ? ValueOrErrors.Default.throwOne(
+        //           Errors.Default.singleton(["not a ValueOption", v]),
+        //         )
+        //       : !v.isSome
+        //       ? ValueOrErrors.Default.return([])
+        //       : traverseOptionValue(v.value);
+        // }
         case "record": {
           const traverseRecordFields = t.fields.map((f) =>
             self(lookupName, typesMap, f),
@@ -69,207 +72,171 @@ export const PredicateValueExtractor = {
                   ),
                 );
         }
-        case "application": {
-          switch (t.value) {
-            case "SingleSelection": {
-              const traverseSingleSelection = self(
-                lookupName,
-                typesMap,
-                t.args[0],
-              );
-              return (v) =>
-                !PredicateValue.Operations.IsOption(v)
-                  ? ValueOrErrors.Default.throwOne(
-                      Errors.Default.singleton([
-                        "not a ValueOption (from SingleSelection)",
-                        v,
-                      ]),
-                    )
-                  : !v.isSome
-                    ? ValueOrErrors.Default.return([])
-                    : traverseSingleSelection(v.value);
-            }
-            case "MultiSelection": {
-              // multi selection only has 1 arg type, which is the same for all the selcted elements
-              const traverseMultiSelectionField = self(
-                lookupName,
-                typesMap,
-                t.args[0],
-              );
-              return (v: PredicateValue) =>
-                !PredicateValue.Operations.IsRecord(v)
-                  ? ValueOrErrors.Default.throwOne(
-                      Errors.Default.singleton([
-                        "not a ValueRecord (from MultiSelection)",
-                        v,
-                      ]),
-                    )
-                  : ValueOrErrors.Operations.All(
-                      List(
-                        v.fields
-                          .entrySeq()
-                          .map(([_, field]) =>
-                            traverseMultiSelectionField(field),
-                          ),
-                      ),
-                    ).Map((listFailingChecks) =>
-                      listFailingChecks.reduce(
-                        (acc, curr) => [...acc, ...curr],
-                        [] as Array<PredicateValue>,
-                      ),
-                    );
-            }
-            case "Map": {
-              const traverseKey = self(lookupName, typesMap, t.args[0]);
-              const traverseValue = self(lookupName, typesMap, t.args[1]);
-              return (v: PredicateValue) =>
-                !PredicateValue.Operations.IsRecord(v)
-                  ? ValueOrErrors.Default.throwOne(
-                      Errors.Default.singleton([
-                        "not a ValueRecord (from Map)",
-                        v,
-                      ]),
-                    )
-                  : ValueOrErrors.Operations.All(
-                      List(
-                        v.fields
-                          .entrySeq()
-                          .map(([_k, field]) =>
-                            ValueOrErrors.Operations.All(
-                              List(
-                                [traverseKey, traverseValue].map(
-                                  (traverseField) => traverseField(field),
-                                ),
-                              ),
-                            ).Map((listFailingChecks) =>
-                              listFailingChecks.reduce(
-                                (acc, curr) => [...acc, ...curr],
-                                [] as Array<PredicateValue>,
-                              ),
+        case "singleSelection": {
+          const traverseSingleSelection = self(lookupName, typesMap, t.args[0]);
+          return (v) =>
+            !PredicateValue.Operations.IsOption(v)
+              ? ValueOrErrors.Default.throwOne(
+                  Errors.Default.singleton([
+                    "not a ValueOption (from SingleSelection)",
+                    v,
+                  ]),
+                )
+              : !v.isSome
+              ? ValueOrErrors.Default.return([])
+              : traverseSingleSelection(v.value);
+        }
+        case "multiSelection": {
+          // multi selection only has 1 arg type, which is the same for all the selcted elements
+          const traverseMultiSelectionField = self(
+            lookupName,
+            typesMap,
+            t.args[0],
+          );
+          return (v: PredicateValue) =>
+            !PredicateValue.Operations.IsRecord(v)
+              ? ValueOrErrors.Default.throwOne(
+                  Errors.Default.singleton([
+                    "not a ValueRecord (from MultiSelection)",
+                    v,
+                  ]),
+                )
+              : ValueOrErrors.Operations.All(
+                  List(
+                    v.fields
+                      .entrySeq()
+                      .map(([_, field]) => traverseMultiSelectionField(field)),
+                  ),
+                ).Map((listFailingChecks) =>
+                  listFailingChecks.reduce(
+                    (acc, curr) => [...acc, ...curr],
+                    [] as Array<PredicateValue>,
+                  ),
+                );
+        }
+        case "map": {
+          const traverseKey = self(lookupName, typesMap, t.args[0]);
+          const traverseValue = self(lookupName, typesMap, t.args[1]);
+          return (v: PredicateValue) =>
+            !PredicateValue.Operations.IsRecord(v)
+              ? ValueOrErrors.Default.throwOne(
+                  Errors.Default.singleton(["not a ValueRecord (from Map)", v]),
+                )
+              : ValueOrErrors.Operations.All(
+                  List(
+                    v.fields
+                      .entrySeq()
+                      .map(([_k, field]) =>
+                        ValueOrErrors.Operations.All(
+                          List(
+                            [traverseKey, traverseValue].map((traverseField) =>
+                              traverseField(field),
                             ),
                           ),
-                      ),
-                    ).Map((listFailingChecks) =>
-                      listFailingChecks.reduce(
-                        (acc, curr) => [...acc, ...curr],
-                        [] as Array<PredicateValue>,
-                      ),
-                    );
-            }
-            case "Sum": {
-              const traverseLeftValue = self(lookupName, typesMap, t.args[0]);
-              const traverseRightValue = self(lookupName, typesMap, t.args[1]);
-              return (v) =>
-                !PredicateValue.Operations.IsSum(v)
-                  ? ValueOrErrors.Default.throwOne(
-                      Errors.Default.singleton(["not a ValueSum", v]),
-                    )
-                  : ValueOrErrors.Operations.All(
-                      List(
-                        [traverseLeftValue, traverseRightValue].map(
-                          (traverseField) => traverseField(v),
+                        ).Map((listFailingChecks) =>
+                          listFailingChecks.reduce(
+                            (acc, curr) => [...acc, ...curr],
+                            [] as Array<PredicateValue>,
+                          ),
                         ),
                       ),
-                    ).Map((listFailingChecks) =>
-                      listFailingChecks.reduce(
-                        (acc, curr) => [...acc, ...curr],
-                        [] as Array<PredicateValue>,
-                      ),
-                    );
-            }
-            case "Option": {
-              const traverseOptionValue = self(lookupName, typesMap, t.args[0]); // TODO: check this
-              return (v: PredicateValue) =>
-                !PredicateValue.Operations.IsOption(v)
-                  ? ValueOrErrors.Default.throwOne(
-                      Errors.Default.singleton(["not a ValueOption", v]),
-                    )
-                  : !v.isSome
-                    ? ValueOrErrors.Default.return([])
-                    : traverseOptionValue(v.value);
-            }
-            case "Tuple": {
-              const traverseTupleFields = t.args.flatMap((f) =>
-                self(lookupName, typesMap, f),
-              );
-              return (v) =>
-                !PredicateValue.Operations.IsTuple(v)
-                  ? ValueOrErrors.Default.throwOne(
-                      Errors.Default.singleton(["not a ValueTuple", v]),
-                    )
-                  : ValueOrErrors.Operations.All(
-                      List(
-                        traverseTupleFields.flatMap((traverseField, idx) =>
-                          traverseField(v.values.get(idx)!),
-                        ),
-                      ),
-                    ).Map((listFailingChecks) =>
-                      listFailingChecks.reduce(
-                        (acc, curr) => [...acc, ...curr],
-                        [] as Array<PredicateValue>,
-                      ),
-                    );
-            }
-            case "Union": {
-              const traverseUnionFields = t.args.map((f) =>
-                self(lookupName, typesMap, f),
-              );
-              return (v) =>
-                !PredicateValue.Operations.IsTuple(v)
-                  ? ValueOrErrors.Default.throwOne(
-                      Errors.Default.singleton([
-                        "not a ValueRecord (from union)",
-                        v,
-                      ]),
-                    )
-                  : ValueOrErrors.Operations.All(
-                      List(
-                        traverseUnionFields.flatMap((traverseField, idx) =>
-                          !v.values.has(idx)
-                            ? []
-                            : [traverseField(v.values.get(idx)!)],
-                        ),
-                      ),
-                    ).Map((listFailingChecks) =>
-                      listFailingChecks.reduce(
-                        (acc, curr) => [...acc, ...curr],
-                        [] as Array<PredicateValue>,
-                      ),
-                    );
-            }
-            case "KeyOf":
-              return (_) => ValueOrErrors.Default.return([]);
-            case "List": {
-              const traverseListField = self(lookupName, typesMap, t.args[0]);
-              return (v) =>
-                !PredicateValue.Operations.IsTuple(v)
-                  ? ValueOrErrors.Default.throwOne(
-                      Errors.Default.singleton([
-                        "not a ValueTuple (from List)",
-                        v,
-                      ]),
-                    )
-                  : ValueOrErrors.Operations.All(
-                      List(v.values.map((v) => traverseListField(v))),
-                    ).Map((listFailingChecks) =>
-                      listFailingChecks.reduce(
-                        (acc, curr) => [...acc, ...curr],
-                        [] as Array<PredicateValue>,
-                      ),
-                    );
-            }
-            case "Table":
-            default:
-              return (_) => ValueOrErrors.Default.return([]);
-          }
+                  ),
+                ).Map((listFailingChecks) =>
+                  listFailingChecks.reduce(
+                    (acc, curr) => [...acc, ...curr],
+                    [] as Array<PredicateValue>,
+                  ),
+                );
         }
-        case "union": {
-          // const traverseUnionFields = t.args.map((f) =>
-          //   self(lookupName, typesMap, f.fields),
-          // );
+        case "sum": {
+          const traverseLeftValue = self(lookupName, typesMap, t.args[0]);
+          const traverseRightValue = self(lookupName, typesMap, t.args[1]);
+          return (v) =>
+            !PredicateValue.Operations.IsSum(v)
+              ? ValueOrErrors.Default.throwOne(
+                  Errors.Default.singleton(["not a ValueSum", v]),
+                )
+              : ValueOrErrors.Operations.All(
+                  List(
+                    [traverseLeftValue, traverseRightValue].map(
+                      (traverseField) => traverseField(v),
+                    ),
+                  ),
+                ).Map((listFailingChecks) =>
+                  listFailingChecks.reduce(
+                    (acc, curr) => [...acc, ...curr],
+                    [] as Array<PredicateValue>,
+                  ),
+                );
+        }
+        case "tuple": {
+          const traverseTupleFields = t.args.map((f) =>
+            self(lookupName, typesMap, f),
+          );
+          return (v) =>
+            !PredicateValue.Operations.IsTuple(v)
+              ? ValueOrErrors.Default.throwOne(
+                  Errors.Default.singleton(["not a ValueTuple", v]),
+                )
+              : ValueOrErrors.Operations.All(
+                  List(
+                    traverseTupleFields.flatMap((traverseField, idx) =>
+                      traverseField(v.values.get(idx)!),
+                    ),
+                  ),
+                ).Map((listFailingChecks) =>
+                  listFailingChecks.reduce(
+                    (acc, curr) => [...acc, ...curr],
+                    [] as Array<PredicateValue>,
+                  ),
+                );
+        }
+        // case "union": {
+        //   const traverseUnionFields = t.args.map((f) =>
+        //     self(lookupName, typesMap, f.fields),
+        //   );
+        //   return (v) =>
+        //     !PredicateValue.Operations.IsTuple(v)
+        //       ? ValueOrErrors.Default.throwOne(
+        //           Errors.Default.singleton([
+        //             "not a ValueRecord (from union)",
+        //             v,
+        //           ]),
+        //         )
+        //       : (ValueOrErrors.Operations.All(
+        //           traverseUnionFields
+        //             .filter((_, k) => v.fields.has(k))
+        //             .map<ValueOrErrors<Array<PredicateValue>, Errors<any>>>(
+        //               (traverseField, k) => traverseField(v.values.get(k)!),
+        //             )
+        //             .valueSeq()
+        //             .toList(),
+        //         ).Map((listFailingChecks) =>
+        //           listFailingChecks.reduce(
+        //             (acc, curr) => [...acc, ...curr],
+        //             [] as Array<PredicateValue>,
+        //           ),
+        //         ));
+        // }
+        case "list": {
+          const traverseListField = self(lookupName, typesMap, t.args[0]);
+          return (v) =>
+            !PredicateValue.Operations.IsTuple(v)
+              ? ValueOrErrors.Default.throwOne(
+                  Errors.Default.singleton(["not a ValueTuple (from List)", v]),
+                )
+              : ValueOrErrors.Operations.All(
+                  List(v.values.map((v) => traverseListField(v))),
+                ).Map((listFailingChecks) =>
+                  listFailingChecks.reduce(
+                    (acc, curr) => [...acc, ...curr],
+                    [] as Array<PredicateValue>,
+                  ),
+                );
+        }
+        case "table": {
           return (_) => ValueOrErrors.Default.return([]);
         }
-        case "table":
         default:
           return (_) =>
             ValueOrErrors.Default.throwOne(
