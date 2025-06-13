3d07596617b7fe9e3589ecdde4ebacaca44f9ec9
Stefano Andriolo <stefano.andriolo@blp-digital.com>
2025-06-13 13:15:52 +0200

    fix: use record id as the chunk map key (#281)
    * fix: use record id as the chunk map keys
    
    * chore: bump version
    
    * chore: prettier

--- frontend/libraries/ballerina-core/src/forms/domains/dispatched-forms/runner/domains/abstract-renderers/table/state.ts
+++ frontend/libraries/ballerina-core/src/forms/domains/dispatched-forms/runner/domains/abstract-renderers/table/state.ts
@@ -137,13 +137,33 @@ export const AbstractTableRendererState = {
           const parsedRow = fromApiRaw(_);
           if (parsedRow.kind == "errors") {
             console.error(parsedRow.errors.toJS());
-            return [key, PredicateValue.Default.record(OrderedMap())];
+            return [
+              key.toString(),
+              PredicateValue.Default.record(OrderedMap()),
+            ];
           }
           if (!PredicateValue.Operations.IsRecord(parsedRow.value)) {
             console.error("Expected a record");
-            return [key, PredicateValue.Default.record(OrderedMap())];
+            return [
+              key.toString(),
+              PredicateValue.Default.record(OrderedMap()),
+            ];
           }
-          return [key, parsedRow.value];
+          if (!parsedRow.value.fields.has("Id")) {
+            console.error("Expected a record with 'Id' field");
+            return [
+              key.toString(),
+              PredicateValue.Default.record(OrderedMap()),
+            ];
+          }
+          if (typeof parsedRow.value.fields.get("Id")! !== "string") {
+            console.error("Id must be a string");
+            return [
+              key.toString(),
+              PredicateValue.Default.record(OrderedMap()),
+            ];
+          }
+          return [parsedRow.value.fields.get("Id")! as string, parsedRow.value];
         }),
       ),
   },
