147fdb799f5e66e1e381ca33a999b83fa78feaba
Nick Seagull <github@nickseagull.dev>
2025-09-18 14:15:32 +0200

    feat(map): add Get method to retrieve values by key (#446)

--- backend/libraries/ballerina-core-go/map.go
+++ backend/libraries/ballerina-core-go/map.go
@@ -10,3 +10,14 @@ type Map[k comparable, v any] []KeyValue[k, v]
 func DefaultMap[k comparable, v any]() Map[k, v] {
 	return make([]KeyValue[k, v], 0)
 }
+
+func (m Map[k, v]) Get(key k) *v {
+	var res *v
+	for _, kv := range m {
+		if kv.Key == key {
+			res = &kv.Value
+			break
+		}
+	}
+	return res
+}
